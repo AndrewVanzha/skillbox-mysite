be24754f0916ad67c890b2c65ad7c1337a6927ed
Andrew <av199903@mail.ru>
2019-06-07 05:18:03 +0300

    верстка адаптивного заголовка
    сделал заголовок адаптивным, подключил всплывающее меню

--- popup-6.js
+++ popup-6.js
@@ -1,26 +1,32 @@
-setTimeout('askExchangeRate();', 10);
-
-function askExchangeRate() {
-  $.get(
-    'http://data.fixer.io/api/latest', {
-      'access_key': '7556a4a9e24d262b95e2bdc7193a5b65'
-    },
-    function (response) {
-      console.log(response);
-      var exch = response.rates.RUB / response.rates.USD;
-      $('.exchange-usd').text(exch.toPrecision(4));
-      exch = response.rates.RUB;
-      $('.exchange-eur').text(exch.toPrecision(4));
-    });
-};
-
 var windowWidth;
+$(window).resize(function () {
+  windowWidth = $(window).width();
+  //console.log(windowWidth);
+});
+
+$(function() {
+  $('.left-header-block').click(function() {
+    windowWidth = $(window).width();
+    if (windowWidth < 1250) {
+      $('.header-line').append(
+      '<div class="popup-box">' +
+      '<div class="nav-list">' +
+      '<li><a href="#">Услуги</a></li>' +
+      '<li><a href="#">Портфолио</a></li>' +
+      '<li><a href="#">Стоимость</a></li>' +
+      '</div>' +
+      '</div>');
+    }
+  });
+});
+
+
 $(window).resize(function () {
   windowWidth = $(window).width();
   if ($('div').is('.popup-menu') && windowWidth > 865) {
     $('.popup-menu').css('display', 'none'); // убрать всплывающее меню
     //$('.popup-menu').hide();
-    $('.burger').css('background-image', 'url("img/burger-sign.png")'); // .burger в исходное состояние
+    $('.burge').css('background-image', 'url("img/burger-sign.png")'); // .burger в исходное состояние
   }
   if (windowWidth > 865) {
     $('.additional-input').hide(); // убрать дополнительный input
